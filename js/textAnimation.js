1e5d45ec8b0444a9f7e5a9514c465f2e5e1f52a8
Ray-linux <ranirava52@gmail.com>
2023-11-02 22:45:31 +0530

    first commit

---
+++ js/textAnimation.js
@@ -0,0 +1,37 @@
+// set up text to print, each item in array is new line
+var aText = new Array(
+    "Happy birthday to the most incredible friend! 🎉🎂 Your friendship is a precious gift, and I'm so grateful for every moment we've shared. May your day be filled with love, laughter, and all the joy in the world. 🥳💕 Here's to another year of adventures and unforgettable memories. Cheers to you, the superstar of our lives! 🌟💖"
+    );
+    var iSpeed = 100; // time delay of print out
+    var iIndex = 0; // start printing array at this posision
+    var iArrLength = aText[0].length; // the length of the text array
+    var iScrollAt = 20; // start scrolling up at this many lines
+     
+    var iTextPos = 0; // initialise text position
+    var sContents = ''; // initialise contents variable
+    var iRow; // initialise current row
+     
+    function typewriter()
+    {
+     sContents =  ' ';
+     iRow = Math.max(0, iIndex-iScrollAt);
+     var destination = document.getElementById("typedtext");
+     
+     while ( iRow < iIndex ) {
+      sContents += aText[iRow++] + '<br />';
+     }
+     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+     if ( iTextPos++ == iArrLength ) {
+      iTextPos = 0;
+      iIndex++;
+      if ( iIndex != aText.length ) {
+       iArrLength = aText[iIndex].length;
+       setTimeout("typewriter()", 500);
+      }
+     } else {
+      setTimeout("typewriter()", iSpeed);
+     }
+    }
+    
+    
+    typewriter();
